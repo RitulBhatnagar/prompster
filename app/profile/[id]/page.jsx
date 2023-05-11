78e686a18d3efa5bab208a2f9f8d1b35961b9837
ritul bhatnagar <ritulbhatnagar375@gamil.com>
2023-05-11 23:23:55 +0530

    done

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,26 @@
+'use client'
+import {useEffect, useState} from "react";
+import { useSearchParams } from "next/navigation";
+import Profile from "@components/Profile";
+const UserProfile = ({params}) => {
+  const searchParams = useSearchParams();
+  const userName = searchParams.get("name");
+  const[userPost, setUserPost] = useState([]);
+  useEffect(() => {
+     const fetchPosts = async() => {
+      const response = await fetch(`/api/users/${params?.id}/posts`)
+      const data = await response.json();
+      setUserPost(data);
+     }
+     if(params?.id) fetchPosts();
+  }, [params.id])
+  return (
+    <Profile
+    name = {userName}
+    desc = {`Welcome to ${userName}s personalized proile page, Explore ${userName}`}
+    data = {userPost}
+    >UserProfile</Profile>
+  )
+}
+
+export default UserProfile
